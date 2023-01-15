55b49ec86a9ef82941061701a497df7ae9b4a0f7
Amit Swisa <amitswisa5@gmail.com>
2023-01-15 18:01:58 +0200

    Registration Done.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const MysqlManager = require("./MysqlManager.js");
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
+const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -10,20 +11,50 @@ app.use(bodyParser.json());
 // Server Port.
 const port = 6829;
 
-const connection = mysql.createConnection({
+const mysqlManager = new MysqlManager({
   host: "localhost",
   user: "root",
   password: "",
   database: "cosmicrace_db",
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("MYSQL Connected!");
-});
-
 app.listen(port, () => {
   console.log("Server listening on port 6829");
 });
 
-app.post("/userAuthenticate", (req, res) => {});
+app.post("/registration", (req, res) => {
+  // check if any parameter is missing.
+  if (!req.body.username || !req.body.password || !req.body.email) {
+    res.status(401).send("Please send all data required.");
+  }
+
+  // Validate if username is already exist.
+  mysqlManager
+    .query("SELECT Count(username) as num FROM gameusers where username = ?", [
+      req.body.username,
+    ])
+    .then((result) => {
+      if (result[0].num === 1) {
+        res.status(403).send("Username already exist in our database.");
+      } else {
+        // No such user exist -> insert to database.
+        mysqlManager
+          .query(
+            "INSERT INTO gameusers (`username`, `password`, `email`) VALUES (?,?,?)",
+            [req.body.username, req.body.password, req.body.email]
+          )
+          .then((result) => {
+            res.status(200).send("Sign up successfully!");
+          }, res)
+          .catch((err) => res.status(403).send("Error occured!"), res);
+      }
+    }, res)
+    .catch((err) => res.status(403).send("Error occured!"), res);
+});
+
+/*
+    # Login HTTP - POST Method.
+    # Req body: Json of username and password.
+    # Response: TRUE || FALSE -> cardentials.
+*/
+app.post("/login", (req, res) => {});
